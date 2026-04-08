8c2ddfff252d0bb9ca63bc63ee4efcc984bb207a
Roshani1419 <roshanirangrej4@gmail.com>
2026-04-08 14:15:39 +0530

    fix  python version

--- tourist_guide/settings.py
+++ tourist_guide/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'tourist_guide.urls'
@@ -120,12 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join (BASE_DIR , 'staticfiles')
 
-STATICFILES_DIRS = [
-os.path.join(BASE_DIR,'static')]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 # EMAIL CONFIGURATION
