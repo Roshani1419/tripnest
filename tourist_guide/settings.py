7bfeb6c10d8aadbe1f6be515a8b1d1430f66ce93
Roshani1419 <roshanirangrej4@gmail.com>
2026-04-09 13:16:58 +0530

    Fix bad request

--- tourist_guide/settings.py
+++ tourist_guide/settings.py
@@ -12,7 +12,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-local-key')
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
-ALLOWED_HOSTS = ('ALLOWED_HOSTS', '127.0.0.1,localhost','.onrender.com',)
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    ".onrender.com",
+]
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.onrender.com",
+]
 
 # APPLICATIONS
 INSTALLED_APPS = [
